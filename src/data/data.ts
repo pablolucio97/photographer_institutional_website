1387f9c3a7016055c9ddc72fc362fc3daa26e159
Pablo Silva <pablolucio_@hotmail.com>
2022-06-23 09:54:23 -0300

    Initial commit

---
+++ src/data/data.ts
@@ -0,0 +1,97 @@
+export const testimonials = [
+  {
+    url: "/girl1.jpg",
+    name: "Lorena Santos",
+    role: "Designer",
+    ratings: 5,
+    testimonial:
+      "Os treinos são sensacionais, estou treinando em casa desde o inicio da quarentena, e estou adorando!",
+  },
+  {
+    url: "/girl3.jpg",
+    name: "Marcela de Oliveira",
+    role: "Full-stack developer",
+    ratings: 4,
+    testimonial:
+      "Seus treinos fizeram muita diferença no meu corpo e na minha saúde mental nessa Pandemia!",
+  },
+  {
+    url: "/boy1.jpg",
+    name: "Márcio José",
+    role: "Front-end developer",
+    ratings: 5,
+    testimonial:
+      "Os treinos são sensacionais, estou treinando em casa desde o inicio da quarentena, e estou adorando!",
+  },
+  {
+    url: "/girl2.jpg",
+    name: "Lorena Campos",
+    role: "Designer",
+    ratings: 5,
+    testimonial:
+      "Seus treinos fizeram muita diferença no meu corpo e na minha saúde mental nessa Pandemia!",
+  },
+  {
+    url: "/girl4.jpg",
+    name: "Pâmela Silva",
+    role: "Full-stack developer",
+    ratings: 4,
+    testimonial:
+      "Seus treinos fizeram muita diferença no meu corpo e na minha saúde mental nessa Pandemia!",
+  },
+  {
+    url: "/boy2.jpg",
+    name: "Marcos Xavier",
+    role: "Front-end developer",
+    ratings: 5,
+    testimonial:
+      "Os treinos são sensacionais, estou treinando em casa desde o inicio da quarentena, e estou adorando!",
+  },
+  {
+    url: "/boy3.jpg",
+    name: "Carlos Santana",
+    role: "Designer",
+    ratings: 5,
+    testimonial:
+      "Os treinos são sensacionais, estou treinando em casa desde o inicio da quarentena, e estou adorando!",
+  },
+  {
+    url: "/boy4.jpg",
+    name: "Paulo Henrique",
+    role: "Full-stack developer",
+    ratings: 4,
+    testimonial:
+      "Seus treinos fizeram muita diferença no meu corpo e na minha saúde mental nessa Pandemia!",
+  },
+  {
+    url: "/boy5.jpg",
+    name: "Bruno Simões",
+    role: "Front-end developer",
+    ratings: 5,
+    testimonial:
+      "Os treinos são sensacionais, estou treinando em casa desde o inicio da quarentena, e estou adorando!",
+  },
+];
+
+export const faqData = [
+  {
+    title: "O acesso é vitalício?",
+    content: "Sim. O acesso ao treinamento é vitalício.",
+  },
+  {
+    title: "De onde posso acessar o treinamento?",
+    content: "Você pode acessar o treinamento de qualquer dispositivo com conexão com a internet.",
+  },
+  {
+    title: "Vou ter suporte para dúvidas?",
+    content: "Sim. Você tera direito a suporte de segunda à sexta, exceto feriados.",
+  },
+  {
+    title: "Existe um grupo de apoio?",
+    content: "Sim. Ao adquirir o treinamento, você terá livre acesso ao nosso grupo do Whatsapp.",
+  },
+  {
+    title: "Posso pedir reembolso?",
+    content: "Sim. Caso você não goste do treinamento por qualquer motivo, você tem 7 dias úteis para pedir reembolso.",
+  },
+];
---
+++ src/styles/index.ts
@@ -0,0 +1,49 @@
+import styled from "styled-components";
+
+export const BannerSection = styled.section`
+  display: flex;
+`;
+
+export const IntroductionSection = styled.section`
+  display: flex;
+`;
+
+export const MetricsSection = styled.section`
+  display: flex;
+`;
+
+export const TestimonialsSection = styled.section`
+  display: flex;
+`;
+
+export const FaqSection = styled.section`
+  display: flex;
+`;
+
+export const PurchaseSection = styled.section`
+  display: flex;
+`;
+
+export const BannerSectionContainer = styled.div`
+  display: flex;
+`;
+
+export const IntroductionSectionContainer = styled.div`
+  display: flex;
+`;
+
+export const MetricsSectionContainer = styled.div`
+  display: flex;
+`;
+
+export const TestimonialSectionContainer = styled.div`
+  display: flex;
+`;
+
+export const FaqSectionSectionContainer = styled.div`
+  display: flex;
+`;
+
+export const PurchaseSectionSectionContainer = styled.div`
+  display: flex;
+`;
