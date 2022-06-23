f84c29797d8efd08048a5d379400045c737ded84
Pablo <pablolucio_@hotmail.com>
2022-06-23 10:47:36 -0300

    IntroductionSection created

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,48 @@
 import type { NextPage } from 'next'
+import { Title } from '../components/Typography/Title'
+import { Text } from '../components/Typography/Text'
+import { BannerSection, BannerSectionContainer, Container, IntroductionSection, IntroductionSectionContainer, IntroductionSectionContentContainer, IntroductionSectionImageContainer } from '../styles'
+import { NextImage } from '../components/Next/NextImage'
+import { RevealFade } from '../components/Animations/RevealFade'
 
 const Home: NextPage = () => {
 
   return (
-    <>
-    </>
+    <Container>
+      <BannerSection id='home'>
+        <BannerSectionContainer>
+        </BannerSectionContainer>
+      </BannerSection>
+      <IntroductionSection>
+        <IntroductionSectionContainer>
+          <IntroductionSectionContentContainer>
+            <Title
+              content='Sobre mim'
+            />
+            <RevealFade>
+              <Text
+                content='Meu nome é Danilo Sacramento e sou apaixonado com fotofragia desde criança. Descobri o meu talento com fotografia aos 11 anos e desde então decidi que isso seria meu trabalho.'
+              />
+              <Text
+                content='Ao longo de 23 anos de profissão, construí uma grande carreira. Tenho todo o empenho para  entregar o máximo que a fotografia pode oferecer.'
+              />
+              <Text
+                content='Trabalho com fotografia de eventos, cerimônias, festas e muito mais, desde o ensaio até ao tratamento correto das fotos e entrega para o cliente onde todo o processo é feito em meu próprio studio.'
+              />
+            </RevealFade>
+          </IntroductionSectionContentContainer>
+          <IntroductionSectionImageContainer>
+            <RevealFade>
+              <NextImage
+                imgUrl='/images/photographer2.jpg'
+                width={644}
+                height={437}
+              />
+            </RevealFade>
+          </IntroductionSectionImageContainer>
+        </IntroductionSectionContainer>
+      </IntroductionSection>
+    </Container>
   )
 }
 
--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,11 +1,74 @@
 import styled from "styled-components";
 
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`;
+
 export const BannerSection = styled.section`
   display: flex;
+  width: 100%;
+  min-height: 40vh;
+`;
+
+export const BannerSectionContainer = styled.div`
+  width: 1024px;
+  margin: 0 auto;
+  background-image: url("/images/photographer1.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
 `;
 
 export const IntroductionSection = styled.section`
   display: flex;
+  width: 100%;
+  min-height: 80vh;
+  background-color: ${({ theme }) => theme.colors.white100};
+`;
+
+export const IntroductionSectionContainer = styled.div`
+  display: flex;
+  width: 1900px;
+  margin: 0 auto;
+  padding: 40px;
+  @media (max-width: 1080px) {
+    flex-direction: column;
+  }
+`;
+export const IntroductionSectionContentContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 40px;
+  width: 50%;
+  & h1 {
+    color: ${({ theme }) => theme.colors.black100};
+    font-weight: 800;
+  }
+
+  & p {
+    color: ${({ theme }) => theme.colors.black500};
+    margin: 24px auto;
+  }
+  @media (max-width: 1080px) {
+    width: 100%;
+    padding: 16px;
+    & h1 {
+      text-align: center;
+    }
+  }
+`;
+export const IntroductionSectionImageContainer = styled.div`
+  display: flex;
+  align-items: center;
+  width: 50%;
+  padding: 40px;
+  width: 50%;
+  @media (max-width: 1080px) {
+    width: 100%;
+    padding: 0 16px;
+    justify-content: center;
+  }
 `;
 
 export const MetricsSection = styled.section`
@@ -21,14 +84,6 @@ export const FaqSection = styled.section`
 `;
 
 export const PurchaseSection = styled.section`
-  display: flex;
-`;
-
-export const BannerSectionContainer = styled.div`
-  display: flex;
-`;
-
-export const IntroductionSectionContainer = styled.div`
   display: flex;
 `;
 
